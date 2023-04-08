f029616db050c3cf3fa4ccc44dab8dda055207a0
MedetGaidarov <mgaiidarov@gmail.com>
2023-04-09 04:33:34 +0500

    react-router-dom was added, ProductPage was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import ProductList from './components/ProductList/ProductList';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductsPage from './pages/ProductPage/ProductPage';
 
 function App() {
-  return (
-    <div className="App">
+
   
+  return (
+    <Router>
+    <div className="App">
+    
+        <Routes>
+          <Route path="/" element={<ProductsPage />} />
+=        </Routes>
     </div>
+    </Router>
   );
 }
 
